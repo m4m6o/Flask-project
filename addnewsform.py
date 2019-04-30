1d9163745475671abcfc9c860c4986f23cf20481
m4m6o <45889084+m4m6o@users.noreply.github.com>
2019-04-30 08:28:22 +0600

    Delete addnewsform.py

--- addnewsform.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
- 
-class AddNewsForm(FlaskForm):
-    title = StringField('Тема', validators=[DataRequired()])
-    content = TextAreaField('Текст', validators=[DataRequired()])
-    submit = SubmitField('Опубликовать')
